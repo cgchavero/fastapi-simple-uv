8b17ce291ad04f8b863074b0f0a16321e57f0f66
Cesar granjeno <cesar.granjeno@dtcco.io>
2025-12-10 13:33:20 -0600

    Add GitHub Actions deploy pipeline fix & root route v3

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def main():
-    return "Hello from fastapi-simple-uv! v2"
+    return "Hello from fastapi-simple-uv! v3"
 
 @app.get("/health")
 async def health_check():
